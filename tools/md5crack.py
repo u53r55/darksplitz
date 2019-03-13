3c6b9ee7e614cafb4ad8ea1cbdd4a74143fa7af9
koboi137 <koboibackbox@gmail.com>
2019-03-13 12:50:28 +0000

    update md5crack

--- tools/md5crack.py
+++ tools/md5crack.py
@@ -10,6 +10,7 @@ def md5crack(hash):
         print('[!] Hash invalid...')
         return False
 
+    requests.packages.urllib3.disable_warnings()
     user_agent = {'User-agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'}
     print('[*] Cracking from nitrxgen')
     try: req = requests.get('http://www.nitrxgen.net/md5db/' + hash, headers = user_agent, timeout = 5).text
@@ -17,35 +18,28 @@ def md5crack(hash):
     if req != '':
         print('[+] Found : ' + req)
     else:
-        print('[*] Cracking from lea.kz')
-        try: req = requests.get('https://lea.kz/api/hash/' + hash, headers = user_agent, timeout = 5).text
+        print('[*] Cracking from hashtoolkit')
+        try: req = requests.get('http://hashtoolkit.com/reverse-hash?hash=' + hash, headers = user_agent, timeout = 5).text
         except: req = 'onyon'
-        if '404 Not Found' not in req:
-            j = json.loads(req)
-            print('[+] Found : ' + j['password'])
+        if 'Hashes for:' in req:
+            pasw = search('Hashes for: <code>(.*?)</code>', req).group(1)
+            print('[+] Found : ' + pasw)
         else:
-            print('[*] Cracking from hashtoolkit')
-            try: req = requests.get('http://hashtoolkit.com/reverse-hash?hash=' + hash, headers = user_agent, timeout = 5).text
+            print('[*] Cracking from gromweb')
+            try: req = requests.get('https://md5.gromweb.com/?md5=' + hash, headers = user_agent, timeout = 5).text
             except: req = 'onyon'
-            if 'Hashes for:' in req:
-                pasw = search('Hashes for: <code>(.*?)</code>', req).group(1)
+            if 'was succesfully' in req:
+                pasw = search('<em class="long-content string">(.*?)</em></p>', req).group(1)
                 print('[+] Found : ' + pasw)
             else:
-                print('[*] Cracking from gromweb')
-                try: req = requests.get('https://md5.gromweb.com/?md5=' + hash, headers = user_agent, timeout = 5).text
+                print('[*] Cracking from my-addr')
+                x = random.choice(range(0, 30))
+                y = random.choice(range(0, 30))
+                data = {'md5': hash, 'x': x, 'y': y}
+                try: req = requests.post('http://md5.my-addr.com/md5_decrypt-md5_cracker_online/md5_decoder_tool.php', data = data, headers = user_agent, timeout = 5).text
                 except: req = 'onyon'
-                if 'was succesfully' in req:
-                    pasw = search('<em class="long-content string">(.*?)</em></p>', req).group(1)
+                if 'Hashed string' in req:
+                    pasw = search('Hashed string</span>: (.*?)</div>', req).group(1)
                     print('[+] Found : ' + pasw)
                 else:
-                    print('[*] Cracking from my-addr')
-                    x = random.choice(range(0, 30))
-                    y = random.choice(range(0, 30))
-                    data = {'md5': hash, 'x': x, 'y': y}
-                    try: req = requests.post('http://md5.my-addr.com/md5_decrypt-md5_cracker_online/md5_decoder_tool.php', data = data, headers = user_agent, timeout = 5).text
-                    except: req = 'onyon'
-                    if 'Hashed string' in req:
-                        pasw = search('Hashed string</span>: (.*?)</div>', req).group(1)
-                        print('[+] Found : ' + pasw)
-                    else:
-                        print('[+] Hash not found...')
+                    print('[+] Hash not found...')
