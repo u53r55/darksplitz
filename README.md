74ecea0cbdf5f51ade9b28bdf1d56e137ca4a648
Koboi Backbox <koboibackbox@gmail.com>
2019-03-27 17:45:34 +0000

    add termux installation

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 This tools is continued from Nefix, DirsPy and Xmasspy project.
 
 # Installation
-Will work fine in the debian shade operating system, like Backbox or kali linux.
+Will work fine in the debian shade operating system, like Backbox, Ubuntu or Kali linux.
 1. ```$ git clone https://github.com/koboi137/darksplitz```
 2. ```$ cd darksplitz/```
 3. ```$ sudo ./install.sh```
@@ -29,6 +29,20 @@ Will work fine in the debian shade operating system, like Backbox or kali linux.
 - Vhost scanner (bypass cloudflare)
 - Mass bruteforce (wordpress)
 - Interactive msfrpc client
+
+# Exploit web application
+- plUpload file upload
+- jQuery file upload (CVE-2018-9206)
+- Laravel (.env)
+- sftp-config.json (misc)
+- Wordpress register (enable)
+- elfinder file upload
+- Drupal 7 exploit (CVE-2018-7600)
+- Drupal 8 exploit (CVE-2018-7600)
+- com_fabrik exploit (joomla)
+- gravityform plugin file upload (wordpress)
+- geoplace3 plugin file upload (wordpress)
+- peugeot-music plugin file upload (wordpress)
 
 # Notes
 This tool will work fine under root, because scapy module and other need root user to access more features.
--- install.sh
+++ install.sh
@@ -1,24 +1,46 @@
 #!/usr/bin/env bash
 
-if [[ "$UID" != "0" ]]; then
-  echo "This script must be run as root !!!"
-  exit
-fi
-apt-get -y install masscan libffi-dev python3-dev python3-pip libssl-dev host nmap
-pip3 install requests
-pip3 install PySquashfsImage
-pip3 install ropper
-pip3 install fake_useragent
-pip3 install netaddr
-pip3 install scapy
-pip3 install netifaces
-pip3 install psutil
-pip3 install --upgrade pip
-python3 -c "import metasploit.msfrpc" 2&> /dev/null
-if [[ "$?" != "0" ]]; then
-  git clone https://github.com/koboi137/pymetasploit
-  cd pymetasploit
-  python3 setup.py install
-  cd ..
-  rm -rf pymetasploit
+which pkg > /dev/null 2>&1
+if [ "$?" -eq "0" ]; then
+  pkg install libffi-dev python nmap dnsutils
+  pip3 install requests
+  pip3 install PySquashfsImage
+  pip3 install ropper
+  pip3 install fake_useragent
+  pip3 install netaddr
+  pip3 install scapy
+  pip3 install netifaces
+  pip3 install psutil
+  pip3 install --upgrade pip
+  python3 -c "import metasploit.msfrpc" 2&> /dev/null
+  if [[ "$?" != "0" ]]; then
+    git clone https://github.com/koboi137/pymetasploit
+    cd pymetasploit
+    python3 setup.py install
+    cd ..
+    rm -rf pymetasploit
+  fi
+else
+  if [[ "$UID" != "0" ]]; then
+    echo "This script must be run as root !!!"
+    exit
+  fi
+  apt-get -y install masscan libffi-dev python3-dev python3-pip libssl-dev host nmap
+  pip3 install requests
+  pip3 install PySquashfsImage
+  pip3 install ropper
+  pip3 install fake_useragent
+  pip3 install netaddr
+  pip3 install scapy
+  pip3 install netifaces
+  pip3 install psutil
+  pip3 install --upgrade pip
+  python3 -c "import metasploit.msfrpc" 2&> /dev/null
+  if [[ "$?" != "0" ]]; then
+    git clone https://github.com/koboi137/pymetasploit
+    cd pymetasploit
+    python3 setup.py install
+    cd ..
+    rm -rf pymetasploit
+  fi
 fi
